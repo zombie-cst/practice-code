7bc2682e2a6c773268c78aa68d4e09a00299189a
zombie_cst <ponovamaja@gmail.com>
2025-05-15 13:26:07 +0300

    fix db

--- db_automobiles_rental.py
+++ db_automobiles_rental.py
@@ -6,48 +6,48 @@ def initialize_db():
     #Создание таблицы "Тип кузова"
     cur.execute('''CREATE TABLE IF NOT EXISTS BodyType(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                name TEXT)''')
+                name TEXT NOT NULL)''')
     
     #Создание таблицы "Автомобили"
     cur.execute('''CREATE TABLE IF NOT EXISTS Automobiles(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                Brand TEXT,
-                Model TEXT,
-                YearRelease INTEGER,
-                Fuel TEXT,
-                Speed REAL,
-                TransmissionBox TEXT,
-                Drive TEXT,
-                Color TEXT,
-                Price REAL,
+                Brand TEXT NOT NULL,
+                Model TEXT NOT NULL,
+                YearRelease INTEGER NOT NULL,
+                Fuel TEXT NOT NULL,
+                Speed REAL NOT NULL,
+                TransmissionBox TEXT NOT NULL,
+                Drive TEXT NOT NULL,
+                Color TEXT NOT NULL,
+                Price REAL NOT NULL,
                 BodyType_id INTEGER,
                 FOREIGN KEY (BodyType_id) REFERENCES BodyType(id))''')
     
     #Создание таблицы "Клиенты"
     cur.execute('''CREATE TABLE IF NOT EXISTS Customers(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                FirstName TEXT,
-                LastName TEXT,
-                Patronymic TEXT,
-                Addres TEXT,
-                PhoneNumber TEXT,
-                Email TEXT)''')
+                FirstName TEXT NOT NULL,
+                LastName TEXT NOT NULL,
+                Patronymic TEXT NOT NULL,
+                Addres TEXT NOT NULL,
+                PhoneNumber TEXT NOT NULL,
+                Email TEXT NOT NULL)''')
     
     #Создание таблицы "Сотрудники"
     cur.execute('''CREATE TABLE IF NOT EXISTS Employees(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                FirstName TEXT,
-                LastName TEXT,
-                Patronymic TEXT,
-                PhoneNumber TEXT,
-                Email TEXT)''')
+                FirstName TEXT NOT NULL,
+                LastName TEXT NOT NULL,
+                Patronymic TEXT NOT NULL,
+                PhoneNumber TEXT NOT NULL,
+                Email TEXT NOT NULL)''')
 
     #Создание таблицы "Бронирование"
     cur.execute('''CREATE TABLE IF NOT EXISTS Booking(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                DateIssue TEXT,
-                ReturnDate TEXT,
-                Addres TEXT,
+                DateIssue TEXT NOT NULL,
+                ReturnDate TEXT NOT NULL,
+                Addres TEXT NOT NULL,
                 Automobiles_id INTEGER,
                 Customers_id INTEGER,
                 Employees_id INTEGER,
@@ -58,10 +58,10 @@ def initialize_db():
     #Создание таблицы "Договор"
     cur.execute('''CREATE TABLE IF NOT EXISTS Contract(
                 id INTEGER PRIMARY KEY AUTOINCREMENT,
-                Date TEXT,
-                Rules TEXT,
-                Discounts TEXT,
-                FinalPrice REAL,
+                Date TEXT NOT NULL,
+                Rules TEXT NOT NULL,
+                Discounts TEXT NOT NULL,
+                FinalPrice REAL NOT NULL,
                 Booking_id INTEGER,
                 FOREIGN KEY (Booking_id) REFERENCES Booking(id))''')
     conn.commit()
